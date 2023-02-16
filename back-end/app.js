2d99a26895538d69bc68fafca03d82888c775d46
sadegh <akbarisadegh@gmail.com>
2023-02-16 16:12:37 +0330

    s

--- back-end/app.js
+++ back-end/app.js
@@ -11,7 +11,6 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use((req, res, next) => {
-     console.clear();
   console.log("Request method: " + req.method + " " + req.url);
   next();
 });
